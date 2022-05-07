67ea37f31fb52d896a0f31db13abf5e686c08dc8
Destru1 <68596367+Destru1@users.noreply.github.com>
2022-05-07 13:28:37 +0300

    #1 Fixed problem

--- BankQA/StartUp.cs
+++ BankQA/StartUp.cs
@@ -6,12 +6,22 @@ namespace BankQA
     {
         static void Main(string[] args)
         {
-            
+            decimal balance = 0;
             decimal deposit = decimal.Parse(Console.ReadLine());
 
+            if (deposit < 0)
+            {
+                Console.WriteLine("You cannot add negative money");
+                
+            }
+            else
+            {
+                balance += deposit;
+                Console.WriteLine($"You deposited {balance} $");
+            }
             
-          
-            Console.WriteLine($"You deposited {deposit} $");
+
+
         }
     }
 }
