5351797e34f0d96c6bebf70f41cf8db0f4b0fff8
Destru1 <68596367+Destru1@users.noreply.github.com>
2022-05-07 13:10:19 +0300

    Add project

---
+++ BankQA/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankQA
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            decimal deposit = decimal.Parse(Console.ReadLine());
+
+            
+          
+            Console.WriteLine($"You deposited {deposit} $");
+        }
+    }
+}
